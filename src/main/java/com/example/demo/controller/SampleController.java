3cec69440f31e562b7ab0e254f9a51d7a098ab41
minhe2810 <minhe2810@naver.com>
2024-04-15 18:19:30 +0900

    sample controller 작성

--- src/main/java/com/example/demo/controller/SampleController.java
+++ src/main/java/com/example/demo/controller/SampleController.java
@@ -1,14 +1,30 @@
 package com.example.demo.controller;
 
-import org.apache.ibatis.session.SqlSessionFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class SampleController {
 	
+
 	@GetMapping("/")
-	public String hello() {
-		return "hello world"; 
+	public ModelAndView sample() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("sample");
+		return mv;
 	}
+		
+	
+	@GetMapping("/v1")
+	public String sample2() {
+		return "sample";	
+	}
+	
+//	@GetMapping("/v3")
+//	public String sample3(Model model) {
+//		model.addAttribute("hello", "hello world");
+//		return "sample";
+//	}
 }
